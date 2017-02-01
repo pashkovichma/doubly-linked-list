9915b01aae733d2ca47b2ed5b83360056f779ba3
pashkovichma <pashkovichma@gmail.com>
2017-02-02 00:11:55 +0300

    doubly-linked-list/fixed:class node

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,11 +1,4 @@
 const Node = require('./node');
-class Node {
-    constructor(data = null, prev = null, next = null) {
-        this.data = data;
-        this.prev = prev;
-        this.next = next;
-    }
-}
 
 class LinkedList {
     constructor() {
